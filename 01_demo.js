baabcfa2fd0f015fc04be72fe4991942dc97c60d
douyc9528 <douyc@itnova.com.cn>
2020-07-11 02:13:15 +0800

    dep2sss

--- 01_demo.js
+++ 01_demo.js
@@ -12,7 +12,7 @@ var New={
              <div>
              <div class="dd1"><ul><li><h2>新闻条目1</h2></li></ul></div>
             <div class="dd1"><ul><li><h2>新闻条目2</h2></li></ul></div>
-            <div class="dd1"><ul><li><h2>新闻条目depwwws</h2></li></ul></div> 
+            <div class="dd1"><ul><li><h2>新闻条目</h2></li></ul></div> 
                </div>  
             `
 }
